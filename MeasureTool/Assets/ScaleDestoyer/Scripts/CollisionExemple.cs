2bd85c7b6797e2586bb05ead21a45db5e24f94a8
Unknown <francoisjanssens.pro@gmail.com>
2018-01-12 16:27:51 +0100

    Add script that allows to destroy objects by size

---
+++ MeasureTool/Assets/ScaleDestoyer/Scripts/CollisionExemple.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionExemple : MonoBehaviour {
+
+    public GameObject _A;
+    public GameObject _B;
+
+    void OnCollisionEnter()
+    {
+        DestroyObjectRapportTaille.DestroyObjectTaille(_A, _B);
+	}
+	
+	
+}
